21ad129999521caea95a8d6860780351ad959f07
gabrielmullerds8-beep <gabrielmullerds8@gmail.com>
2026-06-05 16:48:55 -0300

    Att APP

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://zlejwerphalumabowdln.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_mDhqS_-dKAXaXY_XvO1Bwg_6kdPdT4c"
+  SUPABASE_URL: "https://iysvzrplixdajxmungwr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_hqmf9YH25_Ccd-Oqgt2Qcg_QmCuMZGU"
 };
